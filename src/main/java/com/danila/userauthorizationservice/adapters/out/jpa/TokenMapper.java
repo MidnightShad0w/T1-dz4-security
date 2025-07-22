82095fed3770085f66e73c79fbfbdd26edcf5f81
MidnightShad0w <samsonchik2406@mail.ru>
2025-07-22 17:13:37 +0300

    mapper deleted

--- src/main/java/com/danila/userauthorizationservice/adapters/out/jpa/TokenMapper.java
+++ src/main/java/com/danila/userauthorizationservice/adapters/out/jpa/TokenMapper.java
@@ -2,13 +2,28 @@ package com.danila.userauthorizationservice.adapters.out.jpa;
 
 import com.danila.userauthorizationservice.domain.model.RefreshToken;
 import com.danila.userauthorizationservice.infrastructure.persistence.RefreshTokenEntity;
-import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
 @Component
-public interface TokenMapper {
-    RefreshToken toDomain(RefreshTokenEntity entity);
+public class TokenMapper {
+    public RefreshToken toDomain(RefreshTokenEntity e) {
+        if (e == null) return null;
+        return new RefreshToken(
+                e.getId(),
+                e.getUser().getId(),
+                e.getToken(),
+                e.getExpiresAt(),
+                e.isRevoked()
+        );
+    }
 
-    RefreshTokenEntity toEntity(RefreshToken domain);
+    public RefreshTokenEntity toEntity(RefreshToken d) {
+        if (d == null) return null;
+        RefreshTokenEntity e = new RefreshTokenEntity();
+        e.setId(d.id());
+        e.setToken(d.token());
+        e.setExpiresAt(d.expiresAt());
+        e.setRevoked(d.revoked());
+        return e;
+    }
 }
--- src/main/java/com/danila/userauthorizationservice/adapters/out/jpa/UserMapper.java
+++ src/main/java/com/danila/userauthorizationservice/adapters/out/jpa/UserMapper.java
@@ -1,14 +1,50 @@
 package com.danila.userauthorizationservice.adapters.out.jpa;
 
+import com.danila.userauthorizationservice.domain.model.Role;
 import com.danila.userauthorizationservice.domain.model.User;
+import com.danila.userauthorizationservice.infrastructure.persistence.RoleEntity;
 import com.danila.userauthorizationservice.infrastructure.persistence.UserEntity;
-import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-@Component
-public interface UserMapper {
-    User toDomain(UserEntity entity);
+import java.util.Set;
+import java.util.stream.Collectors;
 
-    UserEntity toEntity(User domain);
+@Component
+public class UserMapper {
+    public User toDomain(UserEntity e) {
+        if (e == null) return null;
+        Set<Role> roles = e.getRoles()
+                .stream()
+                .map(RoleEntity::getName)
+                .map(Role::valueOf)
+                .collect(Collectors.toSet());
+
+        return new User(
+                e.getId(),
+                e.getLogin(),
+                e.getPassword(),
+                roles,
+                e.isEnabled()
+        );
+    }
+
+    public UserEntity toEntity(User d) {
+        if (d == null) return null;
+
+        UserEntity e = new UserEntity();
+        e.setId(d.id());
+        e.setLogin(d.login());
+        e.setPassword(d.passwordHash());
+        e.setEnabled(d.enabled());
+
+        Set<RoleEntity> roleEntities =
+                d.roles().stream().map(r -> {
+                    RoleEntity re = new RoleEntity();
+                    re.setName(r.name());
+                    return re;
+                }).collect(Collectors.toSet());
+
+        e.setRoles(roleEntities);
+        return e;
+    }
 }
